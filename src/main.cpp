1ec9cba8b2d657b12101498893f30ebdfde2b3de
Taranvir Waraich <taranvir@me.com>
2017-06-25 14:56:33 -0400

    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,17 @@
 #endif
 
 int main(int argc, char ** argv) {
+    if (argc != 3){
+        return EXIT_FAILURE;
+    }
     char hostname[HOST_NAME_MAX];
     gethostname(hostname, HOST_NAME_MAX);
     std::cout << "Got hostname\n";
-    MessageHub hub(hostname, "192.168.1.179", 5555);
+    MessageHub hub(hostname, argv[1], 5555);
     std::cout << "initialized hub\n";
     hub.run();
-    hub.send("HELLO", "192.168.1.151:5555");
+    sleep(7);
+    hub.send("HELLO", argv[2]);
     std::cout << "sent msg\n";
     sleep(3);
     return EXIT_SUCCESS;
